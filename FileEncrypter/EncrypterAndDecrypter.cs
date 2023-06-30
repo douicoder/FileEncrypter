45fe07af7fe7387bca8d48e4aa99d70cdd37f93d
Your Name <you@example.com>
2023-06-30 14:19:39 +0200

    Add project files.

---
+++ FileEncrypter/EncrypterAndDecrypter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileEncrypter.Class
+{
+    public class EncrypterAndDecrypter
+    {
+        public string EnCode(string input)
+        {
+            try
+            {
+                // -------------------------------------------------------------------------------------
+                //Keys
+                string PrivateKey = Console.ReadLine();
+                string key = "abcdefgh";
+                // -------------------------------------------------------------------------------------
+
+                // --------------------------------------------------------------------------------------
+                //Converting keys and input into byte
+                byte[] PrivateKeyByte = { };
+
+                PrivateKeyByte = Encoding.UTF8.GetBytes(PrivateKey);
+
+                byte[] KeyByte = { };
+                KeyByte = Encoding.UTF8.GetBytes(key);
+
+                byte[] inputtextbyte = { };
+                inputtextbyte = Encoding.UTF8.GetBytes(input);
+                // --------------------------------------------------------------------------------------
+
+                using (DESCryptoServiceProvider dsp = new DESCryptoServiceProvider())
+                {
+                    var strem = new MemoryStream();
+                    var cryptostremobj = new CryptoStream(strem, dsp.CreateEncryptor(KeyByte, PrivateKeyByte), CryptoStreamMode.Write);
+                    cryptostremobj.Write(inputtextbyte, 0, inputtextbyte.Length);
+                    cryptostremobj.FlushFinalBlock();
+                    return Convert.ToBase64String(strem.ToArray());
+
+
+                }
+
+            }
+            catch
+            {
+                return "Error";
+            }
+
+
+        }
+
+        public string DeCode(string input)
+        {
+            try
+            {
+                // -------------------------------------------------------------------------------------
+                //Keys
+                string PrivateKey = Console.ReadLine();
+                string key = "abcdefgh";
+                // -------------------------------------------------------------------------------------
+
+                // --------------------------------------------------------------------------------------
+                //Converting keys and input into byte
+                byte[] PrivateKeyByte = { };
+
+                PrivateKeyByte = Encoding.UTF8.GetBytes(PrivateKey);
+
+                byte[] KeyByte = { };
+                KeyByte = Encoding.UTF8.GetBytes(key);
+
+                byte[] inputtextbyte = { };
+                inputtextbyte = new byte[input.Replace(" ", "+").Length];
+                inputtextbyte = Convert.FromBase64String(input.Replace(" ", "+"));
+
+                // --------------------------------------------------------------------------------------
+                using (DESCryptoServiceProvider dsp = new DESCryptoServiceProvider())
+                {
+                    var strem = new MemoryStream();
+                    var cryptostremobj = new CryptoStream(strem, dsp.CreateDecryptor(KeyByte, PrivateKeyByte), CryptoStreamMode.Write);
+                    cryptostremobj.Write(inputtextbyte, 0, inputtextbyte.Length);
+                    cryptostremobj.FlushFinalBlock();
+                    return Encoding.UTF8.GetString(strem.ToArray());
+
+
+
+                }
+            }
+            catch
+            {
+                return "Error";
+
+            }
+        }
+
+    }
+}
---
+++ FileEncrypter/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FileEncrypter.Class;
+
+Console.Write("Path: ");
+string path =Console.ReadLine();
+
+Console.WriteLine("1.To Encrypt");
+Console.WriteLine("2.To Decrypt");
+string option=Console.ReadLine();
+
+EncrypterAndDecrypter encrypterAndDecrypter = new EncrypterAndDecrypter();
+
+if (option == "1") 
+{
+
+    var data = File.ReadAllText(path);
+
+    string data1 = encrypterAndDecrypter.EnCode(data); ;
+
+    File.WriteAllText(path, data1);
+
+    
+
+}
+else if (option == "2") 
+{
+    var data = File.ReadAllText(path);
+
+    string data1 = encrypterAndDecrypter.DeCode(data); ;
+
+    File.WriteAllText(path, data1);
+}
